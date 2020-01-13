42842b6087900c873fafdaa9c689b8d039afe29b
Nysosis <thenysosis@gmail.com>
2020-01-13 20:07:14 +0000

    Initial publish

---
+++ precache-manifest.c507b721a3208d3404ad71e037aba6af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d7ca9f6a3ca0d7e445b7bd7ee2d7eb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac1f8a9528cbbe05eec9",
+    "url": "/static/css/main.54763551.chunk.css"
+  },
+  {
+    "revision": "a9d0ced92a71505e3532",
+    "url": "/static/js/2.556e2e83.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.556e2e83.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ac1f8a9528cbbe05eec9",
+    "url": "/static/js/main.998e3bcd.chunk.js"
+  },
+  {
+    "revision": "63811bdf41660e75f673",
+    "url": "/static/js/runtime-main.e1ab3bb1.js"
+  }
+]);
